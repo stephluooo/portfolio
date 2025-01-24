e04170e7f939fbe8747c446ff04022408e1f6221
stephluooo <l6luo@ucsd.edu>
2025-01-23 16:55:18 -0800

    js update

--- global.js
+++ global.js
@@ -3,3 +3,36 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+// navLinks = $$('nav a');
+
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname
+//   );
+
+//   if (currentLink) {
+//     currentLink?.classList.add('current');
+//   }
+
+
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'resume/', title: 'Resume' },
+    { url: 'contact/', title: 'Contacts' },
+    { url: 'https://github.com/stephluooo', title: 'GitHub', external: true },
+  ];
+
+  let nav = document.createElement('nav');
+  document.body.prepend(nav);
+
+  for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    if (url.startsWith('https://github.com')){
+        nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+        continue;
+    }
+    url = 'https://github.com/stephluooo' + url;
+    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+  }

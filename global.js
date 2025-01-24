44103e871bd2696134c4c4d2085dcd84e5eccb74
stephluooo <l6luo@ucsd.edu>
2025-01-23 17:25:01 -0800

    js update

--- global.js
+++ global.js
@@ -26,13 +26,38 @@ let pages = [
   let nav = document.createElement('nav');
   document.body.prepend(nav);
 
+  const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
   for (let p of pages) {
     let url = p.url;
     let title = p.title;
-    if (url.startsWith('https://github.com')){
-        nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
-        continue;
+    // TODO create link and add it to nav
+
+
+    console.log('adding to nav');
+    if (url.startsWith('https://github.com')) {
+        // github page
+        url = url
+    } else if (!ARE_WE_HOME && !url.startsWith('http')) {
+        // non home page
+        url = '../' + url;
+    } else {
+        // home page
+        url = 'https://github.com/stephluooo/portfolio' + url
     }
-    url = 'https://github.com/stephluooo' + url;
-    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
-  }
+    
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+    }
+
+    if (a.host != location.host){
+        a.target = "_blank";
+    }
+
+    nav.append(a);
+
+}

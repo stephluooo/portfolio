c5aa9765401f7e9207a0834899c13576e317cb27
stephluooo <l6luo@ucsd.edu>
2025-01-30 15:57:32 -0800

    update proj

--- global.js
+++ global.js
@@ -106,6 +106,12 @@ export async function fetchJSON(url) {
     try {
         // Fetch the JSON file from the given URL
         const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Failed to fetch projects: ${response.statusText}`);
+        }
+        console.log(response);
+        const data = await response.json();
+        return data; 
 
 
     } catch (error) {
@@ -113,4 +119,13 @@ export async function fetchJSON(url) {
     }
 }
 
-fetchJSON("../lib/projects.json")
+export function renderProjects(project, containerElement, headingLevel = 'h2') {
+    containerElement.innerHTML = '';
+    const article = document.createElement('article');
+    article.innerHTML = `
+    <${headingLevel}>${project.title}</${headingLevel}>
+    <img src="${project.image}" alt="${project.title}">
+    <p>${project.description}</p>
+    `;
+    containerElement.appendChild(article);
+}

7a0ba621b955e4ccce014eaeb5f06629a65bf694
stephluooo <l6luo@ucsd.edu>
2025-01-30 17:09:24 -0800

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,7 @@
-import { fetchJSON, renderProjects } from '../global.js';
-const projects = await fetchJSON('../lib/projects.json');
+import { fetchJSON, renderProjects, countProjects } from '../global.js';
+const projects = await fetchJSON('../lib/projects.json')
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
+
+const title = document.querySelector('h1');
+countProjects(projects, title)

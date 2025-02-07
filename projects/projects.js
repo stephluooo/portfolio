679ff24d82e89f8aeacef5320b3f587c05bccbe8
stephluooo <l6luo@ucsd.edu>
2025-02-06 17:12:14 -0800

    d3

--- projects/projects.js
+++ projects/projects.js
@@ -1,7 +1,13 @@
 import { fetchJSON, renderProjects, countProjects } from '../global.js';
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
 const projects = await fetchJSON('../lib/projects.json')
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
 
 const title = document.querySelector('h1');
 countProjects(projects, title)
+
+let arc = d3.arc().innerRadius(0).outerRadius(50)({
+    startAngle: 0,
+    endAngle: 2 * Math.PI,
+  });

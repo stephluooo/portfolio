bf93160d360f27c7ac1c7478c1658555406bbafb
stephluooo <l6luo@ucsd.edu>
2025-02-06 22:17:21 -0800

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -51,3 +51,10 @@ arcs.forEach((arc, idx) => {
       .attr('d', arc)
       .attr('fill', colors(idx)) 
 })
+
+let legend = d3.select('.legend');
+data.forEach((d, idx) => {
+    legend.append('li')
+          .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
+          .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
+})

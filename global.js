75b55466ff4eabda021d67766d483b376e01a7f7
stephluooo <l6luo@ucsd.edu>
2025-02-07 17:38:12 -0800

    Update global.js

--- global.js
+++ global.js
@@ -139,12 +139,15 @@ export function renderProjects(project, containerElement, headingLevel = 'h2') {
         const description = p.description || 'No description available.';
 
         const article = document.createElement('article');
-        const year = p.year || '2024';
+        const year = p.year || 'unkown';
         article.innerHTML = `
         <${headingLevel}>${title}</${headingLevel}>
-        <h3 class="year">c. ${year}</h3>
         <img src="${image}" alt="${title}" onerror="this.src='fallback-image.jpg';">
-        <p>${description}</p>
+        <div>
+            <p>${description}</p>
+            <br />
+            <p>c. ${year}</p>
+        </div>
         `;
 
         containerElement.appendChild(article);

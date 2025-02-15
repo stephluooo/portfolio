edc3b25806441b8facad898e3186440716e8f33d
stephluooo <l6luo@ucsd.edu>
2025-02-14 18:13:17 -0800

    update

--- meta/main.js
+++ meta/main.js
@@ -87,6 +87,29 @@ function displayStats() {
 
 document.addEventListener('DOMContentLoaded', async () => {
   await loadData();
+  createScatterplot();
 });
 
+const width = 1000;
+const height = 600;
 
+const svg = d3
+  .select('#chart')
+  .append('svg')
+  .attr('viewBox', `0 0 ${width} ${height}`)
+  .style('overflow', 'visible');
+
+const yScale = d3.scaleLinear().domain([0, 24]).range([height, 0]);
+
+function createScatterplot() {
+  const dots = svg.append('g').attr('class', 'dots');
+
+  dots
+    .selectAll('circle')
+    .data(commits)
+    .join('circle')
+    .attr('cx', (d) => xScale(d.datetime))
+    .attr('cy', (d) => yScale(d.hourFrac))
+    .attr('r', 5)
+    .attr('fill', 'steelblue');
+}

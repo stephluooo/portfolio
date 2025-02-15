ff52feb28bea2da02b92dbb0d4a92d3f146b8e0c
stephluooo <l6luo@ucsd.edu>
2025-02-14 16:58:08 -0800

    Update main.js

--- meta/main.js
+++ meta/main.js
@@ -2,10 +2,19 @@ let data = [];
 
 async function loadData() {
   data = await d3.csv('./loc.csv');
-  console.log(data);
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
-  console.log('s')
   await loadData();
 });
+
+async function loadData() {
+  data = await d3.csv('loc.csv', (row) => ({
+    ...row,
+    line: Number(row.line), // or just +row.line
+    depth: Number(row.depth),
+    length: Number(row.length),
+    date: new Date(row.date + 'T00:00' + row.timezone),
+    datetime: new Date(row.datetime),
+  }));
+}

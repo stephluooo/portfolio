63c56faf0b28725ab642114b89af579fc8f3f599
stephluooo <l6luo@ucsd.edu>
2025-02-14 18:54:46 -0800

    grid

--- meta/main.js
+++ meta/main.js
@@ -129,6 +129,15 @@ function createScatterplot() {
   xScale.range([usableArea.left, usableArea.right]);
   yScale.range([usableArea.bottom, usableArea.top]);
 
+  // Add gridlines BEFORE the axes
+  const gridlines = svg
+  .append('g')
+  .attr('class', 'gridlines')
+  .attr('transform', `translate(${usableArea.left}, 0)`);
+
+  // Create gridlines as an axis with no labels and full-width ticks
+  gridlines.call(d3.axisLeft(yScale).tickFormat('').tickSize(-usableArea.width));
+
   // Create the axes
   const xAxis = d3.axisBottom(xScale);
   const yAxis = d3.axisLeft(yScale).tickFormat((d) => String(d % 24).padStart(2, '0') + ':00');
